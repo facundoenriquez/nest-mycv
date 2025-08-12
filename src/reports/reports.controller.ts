fa6413f5443e0eb55b477774991cd02666497171
Facundo Enriquez <fenriquez@inym.org.ar>
2025-08-12 13:54:40 -0300

    report post

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Authguard } from '../guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportsService: ReportsService) {}
+
+    @Post()
+    @UseGuards(Authguard)
+    createReport(@Body() body: CreateReportDto) {
+        return this.reportsService.create(body); 
+    }
+}
